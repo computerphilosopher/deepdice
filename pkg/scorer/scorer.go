5e547531e171b9c108809fc7bb5a1ba87bb2504a
computerphilosopher <erlk0nig@naver.com>
2021-07-01 23:14:15 +0900

    CLEANUP: abstract functions

--- pkg/scorer/scorer.go
+++ pkg/scorer/scorer.go
@@ -4,32 +4,36 @@ type Scorable interface {
 	Num() int
 }
 
-func ScoreSingle(scorables []Scorable, num int) int {
-	ret := 0
-	for _, scorable := range scorables {
-		if scorable.Num() == num {
-			ret += num
-		}
-	}
-	return ret
-}
-
-func ScoreNOfAKind(scorables []Scorable, n int) int {
+func numCounter(scorables []Scorable) map[int]int {
 	counter := map[int]int{}
-	sum := 0
-
 	for _, scorable := range scorables {
 		num := scorable.Num()
 		if _, exist := counter[num]; !exist {
 			counter[num] = 0
 		}
 		counter[num] += 1
-		sum += num
 	}
+	return counter
+}
+
+func sum(scorables []Scorable) int {
+	ret := 0
+	for _, scorable := range scorables {
+		ret += scorable.Num()
+	}
+	return ret
+}
+
+func ScoreSingle(scorables []Scorable, num int) int {
+	return num * numCounter(scorables)[num]
+}
+
+func ScoreNOfAKind(scorables []Scorable, n int) int {
+	counter := numCounter(scorables)
 
 	for _, value := range counter {
 		if value >= n {
-			return sum
+			return sum(scorables)
 		}
 	}
 
